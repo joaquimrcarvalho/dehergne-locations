fa8023ec4fcf0f11bb0b6630dab99031125628b5
Joaquim Carvalho <none>
2021-06-09 10:37:33 +0800

    Adds draw_network function

--- notebooks/timelink.py
+++ notebooks/timelink.py
@@ -9,6 +9,12 @@ from itertools import combinations
 from sqlalchemy.orm import Session
 import networkx as nx
 
+
+class community:
+    pass
+
+
+
 def network_from_attribute(e,a: str, mode='cliques'):
     """Generate a network from common attribute values.
 
@@ -81,3 +87,57 @@ def network_from_attribute(e,a: str, mode='cliques'):
                 for ((e1,n1,d1),(e2,n1,d2)) in pairs:
                     G.add_edges_from([(e1,e2,{'date1':d1,'date2':d2,'attribute':a,'value':avalue})]) 
     return G
+
+from bokeh.models import Range1d, Circle, ColumnDataSource, MultiLine, NodesAndLinkedEdges,EdgesAndLinkedNodes
+from bokeh.plotting import figure
+from bokeh.plotting import from_networkx
+from bokeh.palettes import Spectral4
+from bokeh.io import show, save
+import networkx as nx
+
+def draw_network(G,
+        title='Timelink network',
+        hover_tooltips=[("desc","@desc"),("type","@type")],
+        iterations=50,
+        node_colors=('type',
+                    {'person':'green',
+                    'wicky-viagem':'red',
+                    'jesuita-entrada':'blue'}),
+        width=500,height=500):
+
+    """ draws a network using Bokeh and networkx layout
+
+    """   
+   
+    #Establish which categories will appear when hovering over each node
+    HOVER_TOOLTIPS = hover_tooltips
+
+    # Color nodes
+    (cattribute, node_colors) = node_colors
+    for m in list(G.nodes):
+        nt = G.nodes[m][cattribute]
+        nc = node_colors[nt]
+        G.nodes[m]['color'] = nc
+
+    #Create a plot — set dimensions, toolbar, and title
+    plot = figure(tooltips = HOVER_TOOLTIPS,
+                tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom',
+                x_range=Range1d(-20.1, 20.1), y_range=Range1d(-15.1, 15.1), plot_width=width,plot_height=height,title=title)
+
+    #Create a network graph object with spring layout
+    # https://networkx.github.io/documentation/networkx-1.9/reference/generated/networkx.drawing.layout.spring_layout.html
+    network_graph = from_networkx(G, nx.spring_layout, iterations=iterations,scale=20, center=(0, 0))
+
+
+    #Set node size and color
+    network_graph.node_renderer.glyph = Circle(size=15, fill_color='color')
+
+    #Set edge opacity and width
+    network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
+
+    #Add network graph to the plot
+    plot.renderers.append(network_graph)
+    show(plot)
+    fn = save(plot, title=title, filename=f"{title}.html") 
+    print("timelink draw_network plot saved to file: "+fn)
+
